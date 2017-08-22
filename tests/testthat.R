b91c8cdeafff625c82e5b24617c4c48e64e350ed
Nick Golding <goldingn35@gmail.com>
2017-08-22 11:35:15 +1000

    add kernel tests

---
+++ tests/testthat.R
@@ -0,0 +1,5 @@
+library(testthat)
+library(tensorflow)
+library(gretaGP)
+
+test_check("gretaGP")
---
+++ tests/testthat/test_gps.R
@@ -0,0 +1,9 @@
+context('gaussian processes')
+
+test_that('gaussian processes can eb defined in models', {
+
+  skip_if_not(greta:::check_tf_version())
+
+
+})
+
